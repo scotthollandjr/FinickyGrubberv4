53de09bfde4dac3ff719ea0df01d69e026037cfa
scott holland <scotthollandjr@gmail.com>
2016-07-01 14:28:02 -0700

    Add results page with connection to dog activity

--- app/src/main/java/com/example/guest/grubbery/DogActivity.java
+++ app/src/main/java/com/example/guest/grubbery/DogActivity.java
@@ -1,13 +1,31 @@
 package com.example.guest.grubbery;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class DogActivity extends AppCompatActivity {
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class DogActivity extends AppCompatActivity implements View.OnClickListener {
+    @Bind(R.id.searchButton) Button mSearchButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dog);
+        ButterKnife.bind(this);
+
+        mSearchButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if(view == mSearchButton) {
+            Intent intent = new Intent(DogActivity.this, ResultsActivity.class);
+            startActivity(intent);
+        }
     }
 }
--- app/src/main/java/com/example/guest/grubbery/ThankActivity.java
+++ app/src/main/java/com/example/guest/grubbery/ThankActivity.java
@@ -1,13 +1,25 @@
 package com.example.guest.grubbery;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class ThankActivity extends AppCompatActivity {
+    @Bind(R.id.thanksText) TextView mThanksText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_thank);
+        ButterKnife.bind(this);
+
+        Intent intent = getIntent();
+        String username = intent.getStringExtra("username");
+
+        mThanksText.setText("Thank you, " + username + ". Your account has been created.");
     }
 }
