453155b4cd1ad5871764493acd0d84758ced0742
scott holland <scotthollandjr@gmail.com>
2016-07-01 14:55:53 -0700

    Add foods lsitview with adapter to dog activity

--- app/src/main/java/com/example/guest/grubbery/ResultsActivity.java
+++ app/src/main/java/com/example/guest/grubbery/ResultsActivity.java
@@ -2,12 +2,21 @@ package com.example.guest.grubbery;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class ResultsActivity extends AppCompatActivity {
+    private ListView mListView;
+    private String[] foods = new String[] {"Beef Frittata Veg", "Chicken A La Veg", "Duck & Sweet Potato", "Game Bird", "Hasen Duckenpfeffer", "Lamb & Lentil", "Pork & Applesauce", "Pork & Peas", "Salmon A La Veg", "Salmon Tunalini", "Surf & Turf", "Whitefish & Potato"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_results);
+
+        mListView = (ListView) findViewById(R.id.listView);
+
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_expandable_list_item_1, foods);
+        mListView.setAdapter(adapter);
     }
 }
