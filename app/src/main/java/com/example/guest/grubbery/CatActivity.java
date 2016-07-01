c87878815c498a674346e7b31fb7b6e366e85c07
scott holland <scotthollandjr@gmail.com>
2016-07-01 12:10:37 -0700

    Initial commit with basic file structure and styling

---
+++ app/src/main/java/com/example/guest/grubbery/CatActivity.java
@@ -0,0 +1,13 @@
+package com.example.guest.grubbery;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CatActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cat);
+    }
+}
---
+++ app/src/main/java/com/example/guest/grubbery/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.guest.grubbery;
+
+import android.content.Intent;
+import android.graphics.Typeface;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    @Bind(R.id.appNameTextView) TextView mAppNameTextView;
+    @Bind(R.id.dogButton) Button mDogButton;
+    @Bind(R.id.catButton) Button mCatButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        mAppNameTextView = (TextView) findViewById(R.id.appNameTextView);
+        Typeface comfortaaFont = Typeface.createFromAsset(getAssets(), "fonts/fND5XPYKrF2tQDwwfWZJIxampu5_7CjHW5spxoeN3Vs.ttf");
+        mAppNameTextView.setTypeface(comfortaaFont);
+
+        mDogButton.setOnClickListener(this);
+        mCatButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if(view == mDogButton) {
+            Intent intent = new Intent(MainActivity.this, DogActivity.class);
+            startActivity(intent);
+        }
+        if(view == mCatButton) {
+            Intent intent = new Intent(MainActivity.this, CatActivity.class);
+            startActivity(intent);
+        }
+    }
+}
