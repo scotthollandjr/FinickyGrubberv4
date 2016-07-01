bece8630f0ff8a9627856474b26954e33ee80752
scott holland <scotthollandjr@gmail.com>
2016-07-01 15:44:54 -0700

    Connect cat activity to results

--- app/src/main/java/com/example/guest/grubbery/CatActivity.java
+++ app/src/main/java/com/example/guest/grubbery/CatActivity.java
@@ -1,13 +1,31 @@
 package com.example.guest.grubbery;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class CatActivity extends AppCompatActivity {
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class CatActivity extends AppCompatActivity implements View.OnClickListener {
+    @Bind(R.id.searchButton) Button mSearchButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cat);
+        ButterKnife.bind(this);
+
+        mSearchButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if(view == mSearchButton) {
+            Intent intent = new Intent(CatActivity.this, ResultsActivity.class);
+            startActivity(intent);
+        }
     }
 }
