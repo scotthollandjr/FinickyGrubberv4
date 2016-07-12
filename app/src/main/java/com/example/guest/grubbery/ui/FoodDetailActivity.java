667f2b5dbd8aa2b35ccf42b00d086f03ec9896d2
scott holland <scotthollandjr@gmail.com>
2016-07-11 17:31:12 -0700

    Add food detail fragment layout

---
+++ app/src/main/java/com/example/guest/grubbery/ui/FoodDetailActivity.java
@@ -0,0 +1,13 @@
+package com.example.guest.grubbery.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FoodDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_food_detail);
+    }
+}
