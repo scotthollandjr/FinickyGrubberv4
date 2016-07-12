b103768675e7f0092ee8ade63f55bf4982c35f6b
scott holland <scotthollandjr@gmail.com>
2016-07-11 18:10:50 -0700

    Begin setting up detail fragment

--- app/src/main/java/com/example/guest/grubbery/ui/FoodDetailActivity.java
+++ app/src/main/java/com/example/guest/grubbery/ui/FoodDetailActivity.java
@@ -3,6 +3,8 @@ package com.example.guest.grubbery.ui;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.example.guest.grubbery.R;
+
 public class FoodDetailActivity extends AppCompatActivity {
 
     @Override
