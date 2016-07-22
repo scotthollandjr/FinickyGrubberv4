b4d084d8dbcbe26e706d3d128e0c497d6266ce92
scott holland <scotthollandjr@gmail.com>
2016-07-22 14:26:37 -0700

    Add non-functioning type and age spinners

--- app/src/main/java/com/example/guest/grubbery/ui/DogActivity.java
+++ app/src/main/java/com/example/guest/grubbery/ui/DogActivity.java
@@ -5,8 +5,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 
 import com.example.guest.grubbery.Constants;
 import com.example.guest.grubbery.R;
@@ -27,10 +29,12 @@ import butterknife.ButterKnife;
 public class DogActivity extends AppCompatActivity implements View.OnClickListener {
     @Bind(R.id.searchButton) Button mSearchButton;
     @Bind(R.id.brandSelect) EditText mBrandSelect;
-    @Bind(R.id.typeSelect) EditText mTypeSelect;
+    @Bind(R.id.typeSelect) Spinner mTypeSelect;
     @Bind(R.id.withSelect) EditText mWithSelect;
     @Bind(R.id.withoutSelect) EditText mWithoutSelect;
-    @Bind(R.id.ageSelect) EditText mAgeSelect;
+    @Bind(R.id.ageSelect) Spinner mAgeSelect;
+    private Spinner typeSelect;
+    private Spinner ageSelect;
     ArrayList<Food> mFoods = new ArrayList<>();
 
     @Override
@@ -40,16 +44,28 @@ public class DogActivity extends AppCompatActivity implements View.OnClickListen
         ButterKnife.bind(this);
 
         mSearchButton.setOnClickListener(this);
+
+        Spinner typeSpinner = (Spinner) findViewById(R.id.typeSelect);
+        ArrayAdapter<CharSequence> typeAdapter = ArrayAdapter.createFromResource(this, R.array.types_array, android.R.layout.simple_spinner_item);
+        typeAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        typeSpinner.setPrompt("Type:");
+        typeSpinner.setAdapter(typeAdapter);
+
+        Spinner ageSpinner = (Spinner) findViewById(R.id.ageSelect);
+        ArrayAdapter<CharSequence> ageAdapter = ArrayAdapter.createFromResource(this, R.array.dog_age_array, android.R.layout.simple_spinner_item);
+        ageAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        ageSpinner.setPrompt("Age/Style:");
+        ageSpinner.setAdapter(ageAdapter);
     }
 
     @Override
     public void onClick(View view) {
         if(view == mSearchButton) {
             String mBrand = mBrandSelect.getText().toString().trim();
-            String mType = mTypeSelect.getText().toString().trim();
+            String mType = mTypeSelect.getSelectedItem().toString();
             String mWith = mWithSelect.getText().toString().trim();
             String mWithout = mWithoutSelect.getText().toString().trim();
-            String mAge = mAgeSelect.getText().toString().trim();
+            String mAge = mAgeSelect.getSelectedItem().toString();
 
             Intent intent = new Intent(DogActivity.this, FoodListActivity.class);
             intent.putExtra("dogOrCat", Constants.FIREBASE_CHILD_DOG_FOODS);
